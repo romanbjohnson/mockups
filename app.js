e84353c2456457861f7f5828184070bfaa785ffe
romanbjohnson <106290601+romanbjohnson@users.noreply.github.com>
2023-02-10 10:20:59 -0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,38 @@
+const fallDown =  () => {
+    let mario = document.querySelector('.ball')
+    mario.classList.toggle('falling')
+
+
+}
+
+const fallDown1 =  () => {
+    let luigi  = document.querySelector('.luigi')
+    luigi.classList.toggle('one')
+
+    
+}
+
+const fallDown2 =  () => {
+    let wario = document.querySelector('.wario')
+    wario.classList.toggle('falling')
+
+    
+}
+
+
+const fallDown3 =  () => {
+    let princess = document.querySelector('.princess')
+    princess.classList.toggle('falling')
+
+    
+}
+const fallDown4 =  () => {
+    let frogking = document.querySelector('.frogking')
+    frogking.classList.toggle('falling')
+
+    
+}
+
+
+
+
